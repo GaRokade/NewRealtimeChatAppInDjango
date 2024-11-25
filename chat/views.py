e5cc49c7357c77eb4298bf98e57eee5ea8396ade
GaRokade <rokadegayatri11@gmail.com>
2024-11-25 11:09:21 +0530

    update create  views for   register

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import*
 
-
+from django.contrib import messages
+from django.contrib.auth import authenticate,login as auth_login,logout 
 # Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url="/login/")
 def lobby(request):
-    return render(request, 'chat/lobby.html')
+    return render(request, 'lobby.html')
+
+def register(request):
+    if request.method=="POST":
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=User.objects.filter(username=username)
+        if user.exists():
+           messages.info(request,"username is Already Taken")
+           return redirect('/register')
+        user=User.objects.create(
+            first_name=first_name,
+        
+            last_name=last_name,
+            username=username,
+            
+        )
+        
+        user.set_password(password)
+        user.save()
+        messages.info(request,"The account Created Successfully")
+        return redirect('/register/')
+    return render(request,'register.html')   
