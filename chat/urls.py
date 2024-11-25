969812a5506dee0fa776d7f08050f8df81aa6f16
GaRokade <rokadegayatri11@gmail.com>
2024-11-25 11:33:33 +0530

    create urls for login logout and register

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lobby)
+    path('', views.lobby),
+    path('register/', views.register, name='register'),
+        path('Logout/',views.logout_page,name='Logout'),
+    path('login/', views.Login, name='login'),
 ]
