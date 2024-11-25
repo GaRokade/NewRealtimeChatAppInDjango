3fb093dfb781fb8bedeb689f5582648590524c29
GaRokade <rokadegayatri11@gmail.com>
2024-11-25 13:59:35 +0530

    user_list url added

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,17 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lobby),
+    path('', views.lobby, name='lobby'),
+    path('users/', views.user_list_view, name='user_list'),
+    
     path('register/', views.register, name='register'),
         path('Logout/',views.logout_page,name='Logout'),
     path('login/', views.Login, name='login'),
+      # Ensure 'register' is defined
+    # other URL patterns
 ]
+
+   
+
